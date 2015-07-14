ddc3c0f4d04966f759cfe8b5a258c5b1e1ce13bb
bob <bob@BOB-LAPTOP.dtvl.lan>
2015-07-14 17:31:58 +0100

    Incorporating a new assembler

--- src/uk/org/wookey/assembler/AssemblerOutput.java
+++
@@ -1,5 +0,0 @@
-package uk.org.wookey.assembler;
-
-public interface AssemblerOutput {
-	public void setAddr(int address, int value);
-}
--- src/uk/org/wookey/atari/editor/AssemblyCodeEditor.java
+++ src/uk/org/wookey/atari/editor/AssemblyCodeEditor.java
@@ -22,17 +22,13 @@ import javax.swing.text.Document;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+
 import uk.org.wookey.atari.utils.Logger;
 
-public class AssemblyCodeEditor extends GenericEditor {
+public class AssemblyCodeEditor extends RSyntaxTextArea {
 	private static final long serialVersionUID = 1L;
 	private final static Logger _logger = new Logger(AssemblyCodeEditor.class.getName());
-	
-	private final static int LABEL_CONTEXT = NO_CONTEXT + 1;
-	private final static int INSTRUCTION_CONTEXT = NO_CONTEXT + 2;
-	private final static int DIRECTIVE_CONTEXT = NO_CONTEXT + 3;
-	private final static int STD_CONTEXT = NO_CONTEXT + 4;
-	private final static int COMMENT_CONTEXT = NO_CONTEXT + 5;
 	
 	private final static String instructions[] = {
 		"bcc", "bcs", "beq", "bmi", "bne", "bpl", "bvc", "bvs",
@@ -59,73 +55,6 @@ public class AssemblyCodeEditor extends GenericEditor {
 	protected ReservedWordList reservedWords;
 	
 	public AssemblyCodeEditor(EditorStatusBar sb) {
-		super(sb);
-	}
-	
-	protected void setupContexts() {
-		normalAttributes = new SimpleAttributeSet();
-		StyleConstants.setForeground(normalAttributes, Color.black);
-		
-		reservedWordAttributes = new SimpleAttributeSet();
-		StyleConstants.setForeground(reservedWordAttributes, new Color(0x73, 0x04, 0x73));
-		StyleConstants.setBold(reservedWordAttributes, true);
-		
-		seperatorAttributes = new SimpleAttributeSet();
-		StyleConstants.setForeground(seperatorAttributes, Color.red);
-		
-		stringAttributes = new SimpleAttributeSet();
-		StyleConstants.setForeground(stringAttributes, Color.blue);
-		StyleConstants.setItalic(stringAttributes, true);
-		
-		labelAttributes = new SimpleAttributeSet();
-		StyleConstants.setForeground(labelAttributes, new Color(0x0, 0x80, 0xff));
-		StyleConstants.setBold(labelAttributes, true);
-		
-		commentAttributes = new SimpleAttributeSet();
-		StyleConstants.setForeground(commentAttributes, new Color(0x80, 0xff, 0x00));
-		StyleConstants.setItalic(commentAttributes, true);
-		
-		reservedWords = new ReservedWordList(reservedWordAttributes);
-
-		wordSeperators = " \n";
-		quoteCharacters = "";
-	}
-	
-	protected int getContextId(int offset) {
-		int caret = getCaretPosition();
-		int start = getLineStart(caret);
-		int end = getLineEnd(caret);
-		String str = getText();
-		
-		int contextId = LABEL_CONTEXT;
-		
-		for (int i=caret-1; i >= start; i--) {
-			char c = str.charAt(i);
-			
-			if (c == ';') {
-				return COMMENT_CONTEXT;
-			}
-			else if (c == ' ') {
-				contextId = STD_CONTEXT;
-			}
-
-			_logger.logInfo("Check character '" + c + "' == ' '? -> " + contextId);
-		}
-		
-		_logger.logMsg("Context: caret=" + caret + ", start=" + start + ", end=" + end);
-		
-		return contextId;
-	}
-	
-	protected SimpleAttributeSet getAttributeSet(int contextId) {
-		switch (contextId) {
-		case LABEL_CONTEXT:
-			return labelAttributes;
-			
-		case COMMENT_CONTEXT:
-			return commentAttributes;
-		}
-		
-		return normalAttributes;
+		super();
 	}
 }
